babc686ebeaa38fada12a3d7f91b8cefad6b3cc6
Beth Salvatore <bethsalvatore2018@gmail.com>
2018-05-30 21:47:58 -0400

    add image

--- app/main.js
+++ app/main.js
@@ -1,10 +1,22 @@
 // variables for input form
-var zipInput = getelementByID('zipInput');
+var zipCode = getelementByID('zipCode');
 
-// output variables
+// Input form
+var weatherButton = document.getElementById('weatherButton');
+var zipInput = document.getElementById('zipInput');
 
-// error variables
+// Output variables
+var output = document.getElementById('output');
+var cityOutput = document.getElementById('cityOutput');
+var tempK = document.getElementById('temperatureOutputK');
+var tempF = document.getElementById('temperatureOutputF');
+var tempC = document.getElementById('temperatureOutputC');
+var condition = document.getElementById('condition');
+var weatherImage = document.getElementById('weatherImage');
 
+// Error variables
+var error = document.getElementById("error");
+var errorMessage = document.getElementById('errorMessage');
 
 // api variables
 var apiRequest:
@@ -12,48 +24,82 @@ var url = "http://api.openweathermap.org/data/2.5/weather?zip=<zipCode>&us&appid
 var apiId = 4363e3e1747e40f10b2355eee16763f6;
 
 
-var weatherButton = document.getElementById('weatherButton');
 
-var cityOutput = document.('cityOutput');
-var tempK = document.getElementById('temperatureOutputK')
-
+// Waits for page to load before firing
 document.onreadystatechange = function() {
-  if(document.readyState == "interactive"){
-
-    weatherButton.onclick = test;
-    console.log(weatherButton);
-    // add onclick function
-  }
+	if (document.readyState == "interactive") {
+		// Initialize your application or run some code.
+		weatherButton.onclick = getWeather;
+	}
 };
 
-function getWeather(){
+function getWeather() {
 
-  cityOutput.innerHTML = results.name;
-  kel.innerHTML = (results.main.temp) + ' K';
-  fahr.innerHTML = (results.main.temp) + '&deg F';
-  cels.innerHTML = (results.main.temp) + '&deg C';
+	// Set up url for fetching weather data.
+	var url = "http://api.openweathermap.org/data/2.5/weather?zip=<zipCode>&us&appid=<appId>";
+	url = url.replace("<zipCode>", zipInput.value);
+	url = url.replace("<appId>", appId);
+
+	// Code that fetches data from the API URL and stores it in results.
+	apiRequest = new XMLHttpRequest();
+	apiRequest.onload = catchResponse;
+	apiRequest.onerror = httpRequestOnError;
+	apiRequest.open('get', url, true);
+	apiRequest.send();
+}
+
+function httpRequestOnError() {
+	output.style.display = 'none';
+	errorMessage.innerHTML = 'There was a problem reaching the weather API. Try again later.'
+	error.style.display = 'block';
+}
+
+function catchResponse() {
+
+	if (apiRequest.statusText === "OK") {
+
+		var response = JSON.parse(apiRequest.responseText);
+
+		error.style.display = 'none';
+		cityOutput.innerHTML = response.name;
+		tempK.innerHTML = Math.round(response.main.temp) + ' K';
+		tempF.innerHTML = convertKtoF(response.main.temp) + '&deg; F';
+		tempC.innerHTML = convertKtoC(response.main.temp) + '&deg; C';
+		condition.innerHTML = response.weather[0].description;
+		displayImage(convertKtoF(response.main.temp));
+		output.style.display = 'block';
+
+	}
+	else {
+		error.style.display = 'block';
+		errorMessage.innerHTML = apiRequest.statusText;
+	}
+
 }
 
 function convertKtoF(kelvin) {
-  var fahr = kelvin *(9/5) - 459.67;
-  return Math.round(fahr);
-  // or
-  //var tempF = Math.round(kelvin = (9/5) -459.67;
-  // return tempF;
-  //or
-  //return Math.round(kelvin = (9/5) -459.67;)
+	var fahr = kelvin * (9/5) - 459.67;
+	return Math.round(fahr);
 }
 
 function convertKtoC(kelvin) {
-  var cels = kelvin - 273.15;
-  return Math.round(cels);
+	var cel = kelvin - 273.15;
+	return Math.round(cel);
+}
 
-//URL pattern to follow is
+function displayImage(tempF) {
 
-// If zip and API key are good output looks like
+	if (tempF > 85) {
+		weatherImage.src = 'https://goo.gl/c8VxVr';
+	}
+	else if (tempF > 65) {
+		weatherImage.src = 'https://goo.gl/WNV85G';
+	}
+	else if (tempF > 32) {
+		weatherImage.src = 'https://goo.gl/KAbVwR';
+	}
+	else {
+		weatherImage.src = 'https://goo.gl/a4mnmd';
+	}
 
-// If zip is bad output looks like this
-
-
-
-//If API is missing or bad output looks like this
+}
